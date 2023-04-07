054fc7bb11f9c51cb82b9ad6100c86cc62152ca0
Jr <jumair1111@gmail.com>
2023-04-07 17:07:50 +0530

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -16,11 +16,27 @@ console.log(material)
   const randomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0').toUpperCase();
   }
+  const randomNumber = () => {
+    return  Math.floor(Math.random() * 200);
+  }
+
+  const alertFn = ()=>{
+    alert (" It's Coming Soon..💃");  
+  }
   useLayoutEffect(() => {
     materials.McLaren_F1_1993_By_Alex_Ka.color.set(color)
     material.materials.Body.color.set(color)
     material.materials.Frame.color.set(color)
   }, [color])
+
+  useEffect(() => {
+    
+    document.getElementById("comingSoon").addEventListener("mouseover", () => {
+      document.getElementById("comingSoon").style.transform = `translate(${randomNumber()}%, ${randomNumber()}%)`
+    });
+ 
+  }, [])
+  
 
   return (
     <div className="container">
@@ -28,6 +44,7 @@ console.log(material)
         <p>Choose :</p>
         <div onClick={() => setSelectedItem('iphone')}><p>Iphone</p></div>
         <div onClick={() => setSelectedItem('car')}><p>Car</p></div>
+        <div id='comingSoon' onClick={alertFn}><p>Coming Soon..</p></div>
       </div>}
       {selectedItem && <>
         {selectedItem === 'car' ? <Car /> : <Iphone />}
