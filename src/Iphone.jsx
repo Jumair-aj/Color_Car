9a2ef718a793647c1efeeee18019fcc05787ab6f
jumairtw <jumair.jaleel@timesworld.com>
2023-07-06 11:41:56 +0530

    changes

--- src/Iphone.jsx
+++ src/Iphone.jsx
@@ -1,16 +1,18 @@
-import { Canvas } from '@react-three/fiber'
+
 import React, { Suspense } from 'react'
 import { Model } from './Scene2'
 import { Environment, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 const Iphone = () => {
   return (
     <div id='modelCar'>
-        <Canvas shadows >
+        <Canvas>
             {/* <ambientLight/> */}
             {/* <spotLight position={[3,5,4]} intensity={1}/> */}
             {/* <directionalLight color="yellow" position={[0, 0, 5]} /> */}
-            <Environment  near={100} far={1000} resolution={2560} blur={0}/>
+            <Environment  near={100} far={1000} resolution={2560} blur={0} preset='sunset'/>
+
             <Suspense fallback={null}>
                 <Model/>
                 <OrbitControls/>
